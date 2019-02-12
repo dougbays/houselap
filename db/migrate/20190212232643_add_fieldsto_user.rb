d56f886754539deeb77fbddb7dea77b150dda659
Douglas Bay <douglasbays@gmail.com>
2019-02-12 18:42:19 -0500

    Added aditional fields to Devise/User

---
+++ db/migrate/20190212232643_add_fieldsto_user.rb
@@ -0,0 +1,11 @@
+class AddFieldstoUser < ActiveRecord::Migration[5.2]
+  def change
+   add_column :users, :first_name, :string
+   add_column :users, :last_name, :string
+   add_column :users, :date_of_birth, :date
+   add_column :users, :is_seller, :boolean
+   add_column :users, :phone_number, :string
+   add_column :users, :gender, :string
+   add_column :users, :photo, :string
+ end
+end
